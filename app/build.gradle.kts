7067eceaa1c4db2ba3f3a576419ca8d6c0475b73
Alexander Goussas <goussasalexander@gmail.com>
2024-02-24 00:43:01 -0500

    Update version codes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
         minSdk = 19
         targetSdk = 34
         versionCode = 1
-        versionName = "0.1.0"
+        versionName = "0.1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         multiDexEnabled = true
