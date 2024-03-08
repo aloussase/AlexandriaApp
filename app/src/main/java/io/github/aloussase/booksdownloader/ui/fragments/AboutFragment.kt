d63363c4ec0363f488419a2b6b391d60fe8d6bf0
Alexander Goussas <goussasalexander@gmail.com>
2024-03-08 12:01:02 -0500

    Add socials icons to About page
    Closes #11

--- app/src/main/java/io/github/aloussase/booksdownloader/ui/fragments/AboutFragment.kt
+++ app/src/main/java/io/github/aloussase/booksdownloader/ui/fragments/AboutFragment.kt
@@ -1,10 +1,13 @@
 package io.github.aloussase.booksdownloader.ui.fragments
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import io.github.aloussase.booksdownloader.R
+import io.github.aloussase.booksdownloader.databinding.FragmentAboutBinding
 
 class AboutFragment : BaseApplicationFragment(R.layout.fragment_about) {
     override fun onCreateView(
@@ -12,6 +15,20 @@ class AboutFragment : BaseApplicationFragment(R.layout.fragment_about) {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return super.onCreateView(inflater, container, savedInstanceState)
+        super.onCreateView(inflater, container, savedInstanceState)
+
+        val binding = FragmentAboutBinding.inflate(inflater, container, false)
+
+        binding.ibGithub.setOnClickListener { goToUrl("https://github.com/aloussase/alexandria-app") }
+        binding.ibLinkedin.setOnClickListener { goToUrl("https://www.linkedin.com/in/alexander-goussas/") }
+        binding.ibKofi.setOnClickListener { goToUrl("https://ko-fi.com/aloussase") }
+
+        return binding.root
+    }
+
+    private fun goToUrl(url: String) {
+        val uri = Uri.parse(url)
+        val intent = Intent(Intent.ACTION_VIEW, uri)
+        startActivity(intent)
     }
 }
