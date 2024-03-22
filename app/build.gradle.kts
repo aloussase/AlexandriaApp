422c15d277398b3403a0306ce4dc88a660778a12
Alexander Goussas <goussasalexander@gmail.com>
2024-03-22 09:29:01 -0500

    Bump version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "io.github.aloussase.booksdownloader"
         minSdk = 19
         targetSdk = 34
-        versionCode = 4
-        versionName = "0.3.1"
+        versionCode = 5
+        versionName = "0.3.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         multiDexEnabled = true
